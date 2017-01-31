284174b428be3c2bba307db307f6bb36d41bd875
Alexander <alexander.tsukanovvv@gmail.com>
2017-01-31 13:59:56 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,85 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var methodOverride = require('method-override');
+
+
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+mongoose.connect('mongodb://localhost:27017/')
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(methodOverride());
+
+var Todo = mongoose.model('Todo', {
+  text : String
+});
+
+app.get('/api/todos', function(req, res) {
+
+  // use mongoose to get all todos in the database
+  Todo.find(function(err, todos) {
+
+    // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+    if (err)
+      res.send(err)
+
+    res.json(todos); // return all todos in JSON format
+  });
+});
+
+// create todo and send back all todos after creation
+app.post('/api/todos', function(req, res) {
+
+  // create a todo, information comes from AJAX request from Angular
+  Todo.create({
+    text : req.body.text,
+    done : false
+  }, function(err, todo) {
+    if (err)
+      res.send(err);
+
+    // get and return all the todos after you create another
+    Todo.find(function(err, todos) {
+      if (err)
+        res.send(err)
+      res.json(todos);
+    });
+  });
+
+});
+
+// delete a todo
+app.delete('/api/todos/:todo_id', function(req, res) {
+  Todo.remove({
+    _id : req.params.todo_id
+  }, function(err, todo) {
+    if (err)
+      res.send(err);
+
+    // get and return all the todos after you create another
+    Todo.find(function(err, todos) {
+      if (err)
+        res.send(err)
+      res.json(todos);
+    });
+  });
+});
+
+app.get('*', function(req, res) {
+  res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
+app.listen(8080);
+console.log("App listening on port 8080");
+
+
+
